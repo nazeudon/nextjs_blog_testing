323fa6434d4d97b23425837d58e84c67b8d6b636
nazeudon <nazeudon48112@icloud.com>
2021-06-16 22:20:24 +0900

    added blog detail id1 test

---
+++ __tests__/BlogDetail.test.tsx
@@ -0,0 +1,114 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { render, screen, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import { getPage, initTestHelpers } from 'next-page-tester'
+
+initTestHelpers()
+
+process.env.NEXT_PUBLIC_RESTAPI_URL = 'http://127.0.0.1:8000/api'
+
+const handlers = [
+  rest.get(
+    `${process.env.NEXT_PUBLIC_RESTAPI_URL}/get-blogs/`,
+    (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json([
+          {
+            id: 1,
+            title: 'title1',
+            content: 'content1',
+            username: 'username1',
+            tags: [
+              { id: 1, name: 'tag1' },
+              { id: 2, name: 'tag2' },
+            ],
+            created_at: '2021-01-12 12:12:12',
+          },
+          {
+            id: 2,
+            title: 'title2',
+            content: 'content2',
+            username: 'username2',
+            tags: [
+              { id: 1, name: 'tag1' },
+              { id: 2, name: 'tag2' },
+            ],
+            created_at: '2021-01-13 13:13:13',
+          },
+        ])
+      )
+    }
+  ),
+  rest.get(
+    `${process.env.NEXT_PUBLIC_RESTAPI_URL}/get-blogs/1/`,
+    (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          id: 1,
+          title: 'title1',
+          content: 'content1',
+          username: 'username1',
+          tags: [
+            { id: 1, name: 'tag1' },
+            { id: 2, name: 'tag2' },
+          ],
+          created_at: '2021-01-13 13:13:13',
+        })
+      )
+    }
+  ),
+  rest.get(
+    `${process.env.NEXT_PUBLIC_RESTAPI_URL}/get-blogs/2/`,
+    (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          id: 2,
+          title: 'title2',
+          content: 'content2',
+          username: 'username2',
+          tags: [
+            { id: 1, name: 'tag1' },
+            { id: 2, name: 'tag2' },
+          ],
+          created_at: '2021-01-13 13:13:13',
+        })
+      )
+    }
+  ),
+]
+
+const server = setupServer(...handlers)
+
+beforeAll(() => {
+  server.listen()
+})
+afterEach(() => {
+  server.resetHandlers()
+  cleanup()
+})
+afterAll(() => {
+  server.close()
+})
+
+describe('BlogDetailPage Test Cases', () => {
+  it('Should render detailed content of ID 1', async () => {
+    const { page } = await getPage({
+      route: '/posts/1',
+    })
+    render(page)
+    expect(await screen.findByText('title1')).toBeInTheDocument()
+    expect(screen.getByText('content1')).toBeInTheDocument()
+    expect(screen.getByText('by username1')).toBeInTheDocument()
+    expect(screen.getByText('tag1')).toBeInTheDocument()
+    expect(screen.getByText('tag2')).toBeInTheDocument()
+  })
+})
